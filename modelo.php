74d6b2d5e3b91bf74ab82c911eaf326c62061090
velasZen <aritzhu94@gmail.com>
2024-06-06 12:28:32 +0200

    Create modelo.php

---
+++ modelo.php
@@ -0,0 +1,5 @@
+mysql://root:FugGrCWZSpKXmhJjhjlZbJVwixjNHGzm@viaduct.proxy.rlwy.net:23360/railway
+
+mysql -hviaduct.proxy.rlwy.net -uroot -pFugGrCWZSpKXmhJjhjlZbJVwixjNHGzm --port 23360 --protocol=TCP railway
+
+railway connect MySQL
