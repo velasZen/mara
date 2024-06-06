f263110cd2ee55446473a1b777c8f0affacba787
velasZen <aritzhu94@gmail.com>
2024-06-06 13:59:38 +0200

    Update index.php

--- 2082_pure_mix/index.php
+++ 2082_pure_mix/index.php
@@ -1,6 +1,18 @@
 <?php
   require_once(modelo.php);
 
+  if (isset($_GET['accion'])) {
+		$accion = $_GET['accion'];
+	} else {
+		$accion = "add_candle";
+	}
+
+	if (isset($_GET['id'])) {
+		$id = $_GET['id'];
+	} else {
+		$id = 1;
+	}
+
   if($accion == "add_candle"){
     switch ($id):
       case 1:
