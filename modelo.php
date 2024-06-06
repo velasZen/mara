8638007611044c5199b8658bdf9a7ff3cb0ff435
velasZen <aritzhu94@gmail.com>
2024-06-06 12:48:39 +0200

    Update modelo.php

--- modelo.php
+++ modelo.php
@@ -1,5 +1,26 @@
-mysql://root:FugGrCWZSpKXmhJjhjlZbJVwixjNHGzm@viaduct.proxy.rlwy.net:23360/railway
+<?php
+require_once("conexion.php");
+  function addCandle(){
+    $con = conexion();
+  	$stmt = $con->prepare("insert into CANDLES (name, price, height, weight, type, description) values (?,?,?,?,?,?)");
+  	$stmt->bind_param("ssssss", $nom, $pri, $hei, $wei , $ty, $desc);
+  
+  	$nom = $_POST['name'];
+  	$pri = $_POST['price'];
+  	$hei = $_POST['height'];
+    $wei = $_POST['weight'];
+    $ty = $_POST['type'];
+    $desc = $_POST['description'];
+  
+  	$stmt->execute();
+  
+  	if ($stmt->affected_rows) {
+  		echo "Se ha insertado correctamente<br>";
+  	} else {
+  		echo "No se ha insertado la persona<br>";
+  	}
+    
+  }
 
-mysql -hviaduct.proxy.rlwy.net -uroot -pFugGrCWZSpKXmhJjhjlZbJVwixjNHGzm --port 23360 --protocol=TCP railway
 
-railway connect MySQL
+>
