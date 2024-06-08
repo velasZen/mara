ad2026f5f1c7edbf63358cc934c0bd9994595054
velasZen <aritzhu94@gmail.com>
2024-06-08 12:08:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php
   include "modelo.php";
-
-  if (isset($_GET['accion'])) {
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+	addPrueba();
+}else{
+	if (isset($_GET['accion'])) {
 		$accion = $_GET['accion'];
 	} else {
 		$accion = "add_candle";
 	}
-
 	if (isset($_GET['id'])) {
 		$id = $_GET['id'];
 	} else {
 		$id = 1;
 	}
-echo $accio;
-if($accion == "add_candle"){
-	switch ($id):
-	case 1:
-	addPrueba();
-	break;
+	echo $accio;
+	if($accion == "add_candle"){
+		switch ($id):
+		case 1:
+		addPrueba();
+		break;
+	}
 }
 ?>
