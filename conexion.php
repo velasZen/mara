8683d1d277615642ed53a1724a7ee2f5fdc2abe6
velasZen <aritzhu94@gmail.com>
2024-06-08 11:29:31 +0200

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,21 @@
+<?php
+
+	function conexion () {
+		$servidor = "viaduct.proxy.rlwy.net";
+		$bd = "railwey";
+		$user = "root";
+		$password = "FugGrCWZSpKXmhJjhjlZbJVwixjNHGzm";
+
+		$con = mysqli($servidor, $user, $password, $bd);
+
+		if (!$con) {
+			echo "Error de conexión de base de datos <br>";
+			echo "Error número: " . mysqli_connect_errno();
+			echo "Texto error: " . mysqli_connect_error();
+			exit;
+		}
+
+		return $con;
+	}
+
+?>
---
+++ modelo.php
@@ -0,0 +1,43 @@
+<?php
+include "conexion.php";
+  function addCandle(){
+    $con = conexion();
+  	$stmt = $con->prepare("insert into CANDLES (name, price, height, weight, type, description) values (?,?,?,?,?,?)");
+  	$stmt->bind_param("ssssss", $nom, $pri, $hei, $wei , $ty, $desc);
+  
+  	$nom = $_POST['name'];
+  	$pri = $_POST['price'];
+  	$hei = $_POST['height'];
+    $wei = $_POST['weight'];
+    $ty = $_POST['type'];
+    $desc = $_POST['description'];
+  
+  	$stmt->execute();
+  
+  	if ($stmt->affected_rows) {
+  		echo "Se ha insertado correctamente<br>";
+  	} else {
+  		echo "No se ha insertado la persona<br>";
+  	}
+    
+  }
+
+  function addPrueba(){
+    $con = conexion();
+  	$stmt = $con->prepare("insert into PRUEBA (id,nombre) values (?,?)");
+  	$stmt->bind_param("ds",$id, $nom);
+    $id = 0;
+  	$nom = $_GET['name'];
+    
+  	$stmt->execute();
+  
+  	if ($stmt->affected_rows) {
+  		echo "Se ha insertado correctamente<br>";
+  	} else {
+  		echo "No se ha insertado la persona<br>";
+  	}
+    
+  }
+
+
+?>
